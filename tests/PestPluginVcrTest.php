6ebbd85ebef01e94bcb2cfabccd4305558857025
Elhaouari Mohammmed <9967336+mohammed-elhaouari@users.noreply.github.com>
2021-08-28 00:56:39 +0100

    update phpunit.xml

---
+++ tests/PestPluginVcrTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPJuice\PestPluginVcr\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class PestPluginVcrTest extends TestCase
+{
+    public function testAssertsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
