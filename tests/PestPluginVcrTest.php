c19e646ed24cd4b1b27ff12b13f632818866b7fb
Elhaouari Mohammmed <9967336+mohammed-elhaouari@users.noreply.github.com>
2021-08-28 02:59:13 +0100

    add vcr tests

--- tests/PestPluginVcrTest.php
+++ tests/PestPluginVcrTest.php
@@ -1,13 +1,13 @@
 <?php
+
 declare(strict_types=1);
 
 use VCR\VCR;
 use function PHPJuice\PestPluginVcr\vcrTurnOff;
 use function PHPJuice\PestPluginVcr\vcrTurnOn;
 
-beforeAll(function () {
+beforeEach(function () {
     VCR::configure()->setCassettePath(__DIR__."/cassettes");
-    VCR::configure()->enableLibraryHooks(['curl']);
 });
 
 it('intercepts with cassette name', function () {
